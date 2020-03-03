07bba967557f380d47de9d674ef33867418d7d84
FP <FP@PC11>
2020-03-03 14:27:51 +0100

    .

--- boletin 5/src/ejercicio1.java
+++ boletin 5/src/ejercicio1.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
 
 public class ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	ArrayList<String> arrayLetra=new ArrayList<>();
+	arrayLetra.add('N');
+	Persona p1=new Persona("Nicolas","Cainzos",20);
+	Persona p2=new Persona("Pablo","Castro",60);
+	Persona p3=new Persona("Natalia","Lopez",32);
 
+
+	}
+	
+	public static ArrayList ArrayPersona(ArrayList arrayLetra) {
+		ArrayList<Persona> ArrayNombre=new ArrayList<>();
+		
+		return ArrayNombre;
+		
 	}
 
 }
